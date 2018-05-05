9ef2020aa1e427ac746c0430b79b985ea50fcd8b
bawdybytes <bawdybytes@gmail.com>
2018-05-04 23:27:49 -0800

    Sparkly, shiny code. It compiles.

---
+++ CreeperForage/CreeperForageMod.cs
@@ -0,0 +1,17 @@
+﻿using StardewModdingAPI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CreeperForage
+{
+    public class CreeperForageMod : Mod
+    {
+        public override void Entry(IModHelper helper)
+        {
+            this.Monitor.Log("Heya creepers!", LogLevel.Alert);
+        }
+    }
+}
